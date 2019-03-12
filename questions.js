fab6a6943ceafa09dd4e6bb4a42bb5953c473880
Carlos Segoviano <halokid626@hotmail.com>
2019-03-12 03:05:15 -0700

    Add all files

---
+++ questions.js
@@ -0,0 +1,134 @@
+const STORE = [
+  {
+    question: '1. When is Lil Pump\'s birthday?',
+    answers: [
+      'August 17',
+      'July 17',
+      'September 18',
+      'July 19',
+      'August 16'
+      ],
+    correctAnswer: 'August 17',
+    icon: 'https://pbs.twimg.com/media/DFyRfJ9V0AED-GE.jpg:large',
+    icon2: 'https://media1.tenor.com/images/7d9a57342e08b27fed814761ce2818ac/tenor.gif?itemid=8179301',
+    alt: 'lil pump cake',
+    alt2: 'pump mad'
+  },
+  {
+    question: '2. What is Lil Pump\'s real name?',
+    answers: [
+      'Omar Pineiro',
+      'Gazzy Garcia',
+      'Daniel Hernandez',
+      'Stokeley Clevon Goulbourne',
+      'Jahseh Dwayne Ricardo Onfroy'
+      ],
+    correctAnswer: 'Gazzy Garcia',
+    icon: '',
+    alt: 'pump before fame'
+  },
+  {
+    question: '3. How old is Lil Pump?',
+    answers: [
+      '17',
+      '21',
+      '18',
+      '19',
+      '24'
+      ],
+    correctAnswer: '18',
+    icon: '',
+    alt: 'pump party'
+  },
+  {
+    question: '4. Which rapper is Lil Pump\'s best friend',
+    answers: [
+      'XXXTentacion',
+      'Trippie Red',
+      'Lil Uzi Vert',
+      'Bad Bunny',
+      'SmokePurpp'
+      ],
+    correctAnswer: 'SmokePurpp',
+    icon: '',
+    alt: 'SmokePurpp drip'
+  },
+  {
+    question: '5. Which Lil Pump song went number 3 on Billboard?',
+    answers: [
+      'ESSKEETIT',
+      'BOSS',
+      'Flex Like OUU',
+      'Gucci Gang',
+      'Drug Addict',
+      ],
+    correctAnswer: 'Gucci Gang',
+    icon: '',
+    alt: 'Gucci Gang music video'
+  },
+  {
+    question: '6. From which music platform did Lil Pump gain fame?',
+    answers: [
+      'Spotify',
+      'SoundCloud',
+      'Apple Music',
+      'Tidal',
+      'Pandora'
+      ],
+    correctAnswer: 'SoundCloud',
+    icon: '',
+    alt: 'SoundCloud Logo'
+  },
+  {
+    question: '7. From which state is Lil Pump from?',
+    answers: [
+      'Florida',
+      'New York',
+      'California',
+      'Georgia',
+      'Texas'
+      ],
+    correctAnswer: 'Florida',
+    icon: '',
+    alt: 'Florida State'
+  },
+  {
+    question: '8. What is Lil Pump\'s Ethnicity?',
+    answers: [
+      'African',
+      'Mexican',
+      'American',
+      'Columbian',
+      'Other'
+      ],
+    correctAnswer: 'Columbian',
+    icon: '',
+    alt: 'Columbian pump'
+  },
+  {
+    question: '9. Who did Lil Pump have a crush on?',
+    answers: [
+      'iCarly',
+      'Kendall Jenner',
+      'Chantell Jefferies',
+      'Amber Rose',
+      'Tana Mongeau'
+      ],
+    correctAnswer: 'iCarly',
+    icon: '',
+    alt: 'iCarly'
+  },
+  {
+    question: '10. What is the infamous word that Lil Pump uses all the time?',
+    answers: [
+      'Stoopid',
+      'Lean',
+      'ESSKEETIT',
+      'OUU',
+      'Dummy'
+      ],
+    correctAnswer: 'ESSKEETIT',
+    icon: '',
+    alt: 'ESSKEETIT'
+  },
+];
